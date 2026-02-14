7492ba2a92259f9a1427378c3bae334c7dec038f
carlosa789 <carlosazuka@icloud.com>
2026-02-14 12:34:55 +0400

    Implement modal open and close functions
    Added modal functionality to open and close modal.

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-alert("JS მუშაობს 🎉");
+const modal = document.getElementById("modal");
+
+function openModal() {
+  modal.classList.add("active");
+}
+
+function closeModal() {
+  modal.classList.remove("active");
+}
+
+function closeOnBackdrop(e) {
+  if (e.target === modal) {
+    closeModal();
+  }
+}
